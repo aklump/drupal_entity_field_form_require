57c6a8bc977097bb882e7da9676d93c27e9b404b
Aaron Klump <sourcecode@intheloftstudios.com>
2023-05-03 14:40:40 -0700

    Add #required to syntax for more intuitive code read.

--- entity_field_form_require.api.php
+++ entity_field_form_require.api.php
@@ -9,15 +9,18 @@
  *
  * @return array[]
  *   An array keyed by field name.  Each value is an array with keys:
- *   - display_ids string[] One or more form display ids wherein to require this
- *   field in this format ENTITY_TYPE_ID.BUNDLE.MODE.
+ *   - #required array[] Each value has keys:
+ *     - display_ids string[] One or more form display ids wherein to require
+ *     this field in this format ENTITY_TYPE_ID.BUNDLE.MODE.
  */
 function hook_entity_field_form_require_field_info() {
   return [
     'field_worker_signature' => [
-      'display_ids' => [
-        'node.worker_timesheet.approve_worker',
-        'node.worker_timesheet.reject',
+      '#required' => [
+        'display_ids' => [
+          'node.worker_timesheet.approve_worker',
+          'node.worker_timesheet.reject',
+        ],
       ],
     ],
   ];
